dbe22c552d648bf92cf2eff730ddc7edfe4d0c76
Akshay Kumar Sinha <160411667+codebyakshay@users.noreply.github.com>
2025-03-29 10:09:24 +0530

    adding sum logic to the expense summary component

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function ExpensesSummary() {
+export default function ExpensesSummary({ periodName, expenses }) {
+  const expensesSum = expenses.reduce((sum, expenses) => {
+    return sum + expenses.amount;
+  }, 0);
   return (
     <View>
-      <Text>ExpensesSummary</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
